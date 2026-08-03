f941e54cd8f501fcddf9f02c48a2cfbdca1c2dd8
kykyky16 <kwek_yong@bca.gov.sg>
2026-08-03 14:15:57 +0000

    F

---
+++ llm.py
@@ -0,0 +1,6 @@
+import os
+from openai import OpenAI
+import tiktoken
+
+api_key = os.environ["OPENAI_API_KEY"]
+
---
+++ main.py
@@ -0,0 +1,20 @@
+# Set up and run this Streamlit App
+import streamlit as st
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="My Streamlit App"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("Streamlit App")
+
+form = st.form(key="form")
+form.subheader("Prompt")
+
+user_prompt = form.text_area("Enter your prompt here", height=200)
+
+if form.form_submit_button("Submit"):
+    st.toast("User Input Submitted - {user_prompt}")
+    print(f"User Input is {user_prompt}")
