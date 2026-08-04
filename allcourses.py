7a6597f936dff4d1bbe4a3c4a2bd733cfb6e0454
kykyky16 <kwek_yong@bca.gov.sg>
2026-08-04 15:43:51 +0000

    J

--- allcourses.py
+++ allcourses.py
@@ -7,20 +7,19 @@ import pandas as pd
 st.markdown("All Courses")
 st.sidebar.markdown("All Courses")
 
-st.title("Course Catalog")
-
-st.set_page_config(page_title="Full JSON Viewer", layout="wide")
-st.title("Full JSON File Viewer")
-
 json_path = "/workspaces/week6/data/courses-full.json"
 
 with open(json_path, "r", encoding="utf-8") as f:
     data = json.load(f)
 
+st.title("Course Catalog")
 
 for course_name, details in data.items():
     with st.expander(course_name):
         st.table(pd.DataFrame(details.items(), columns=["Attribute", "Value"]))
+
+st.set_page_config(page_title="Full JSON Viewer", layout="wide")
+st.title("Full JSON File Viewer")
 
 st.subheader("Raw JSON")
 st.json(data)
